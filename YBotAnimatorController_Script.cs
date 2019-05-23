3067ed41c88486de79c52e1372d53cf9ee7838c6
Jordance Webb <jordancewebb@yahoo.com>
2019-05-23 17:34:12 -0400

    Add files via upload

---
+++ YBotAnimatorController_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YBotAnimatorController_Script : MonoBehaviour {
+    static Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetBool("isForward", true);
+        }
+        else
+        {
+            anim.SetBool("isForward", false);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("isBackward", true);
+        }
+        else
+        {
+            anim.SetBool("isBackward", false);
+        }
+
+	}
+}
